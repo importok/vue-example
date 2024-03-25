093808b936d51e2ded34efa2a15e2e830d636ee2
Iacovos Constantinou <softius@gmail.com>
2024-03-25 12:09:18 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<script>
+import ImportokWizard from "@importok/vue";
+
+export default {
+    components: {
+        ImportokWizard
+    },
+
+    methods: {
+        /**
+         * Push the provided record to the API
+         * Check https://importok.io/docs/webhooks.html for more details
+         */
+        async saveRecord(record, meta) {
+            return await fetch('https://httpstat.us/200');
+        }
+    },
+
+    data() {
+        return {
+            /**
+             * Import fields to be mapped
+             * Check https://importok.io/docs/fields.html for more details
+             */
+            fields: {
+                first_name: {
+                    label: 'First Name',
+                    transformers: 'capitalize|trim'
+                },
+                last_name: {
+                    label: 'Last Name',
+                    validators: 'required|length:5',
+                    transformers: 'capitalize|trim'
+                },
+                company_name: {
+                    label: 'Company Name',
+                    transformers: 'trim'
+                },
+                email: {
+                    label: 'Email',
+                    validators: 'email|required',
+                    transformers: 'lowercase|trim'
+                },
+                phone: {
+                    label: 'Phone',
+                    validators: 'phone',
+                    transformers: 'replace:-|trim'
+                },
+                country: {
+                    label: 'Country',
+                    validators: 'in:CY,GR,UK',
+                    transformers: 'uppercase|trim',
+                },
+            }
+        };
+    }
+};
+</script>
+<template>
+    <ImportokWizard
+        title="ImportOK Example for Vue"
+        :fields="fields"
+        sample-file="/sample.csv"
+        @record-ready="saveRecord"
+    />
+</template>
